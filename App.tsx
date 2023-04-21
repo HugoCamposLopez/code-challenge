480f5bdb2f83dd20159e9e7671be27235e6150a1
Hugo Antonio Campos Lopez <hugo.campos@atomic32.com>
2023-04-21 16:33:08 -0600

    First commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const App = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Hola mundo</Text>
+    </View>
+  );
+};
