c560914a3d3b1b9245530071edecf8c0c8d95215
HugoCamposLopez <hugo.antonio.campos@gmail.com>
2023-04-23 12:46:34 -0600

    (ADD) LoadingScreen. (UP) fetch in stack. (ADD) FilterButton Component

--- src/containers/HomeContainer/HomeContainer.tsx
+++ src/containers/HomeContainer/HomeContainer.tsx
@@ -1,28 +1,19 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { GenericContainer } from '../../components/Atoms/GenericContainer/GenericContainer';
 import { Header } from '../../components/Molecules/Header/Header';
 import { PointsCard } from '../../components/Molecules/PointsCard/PointsCard';
 import { MovementsComponentList } from '../../components/Molecules/MovementsComponentList/MovementsComponentList';
-import StyledButton from '../../components/Atoms/StyledButton/StyledButton';
-import { ActivityIndicator, View } from 'react-native';
 import { usefetchMovements } from '../../store/action-creators/usefetchMovements';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
-import i18n from '../../i18n';
 import { PropertiesI } from '../../components/Molecules/CardItem/CardItem';
+import FilterButton from '../../components/Molecules/FilterButton/FilterButton';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export const HomeContainer = (): JSX.Element => {
   const { movements } = useSelector((state: RootState) => state);
   const { fetchMovements_execute, loading, success } = usefetchMovements();
   const [isFilter, setIsFilter] = useState(false);
-
-  useEffect(() => {
-    fetchMovements_execute();
-  }, []);
-
-  useEffect(() => {
-    !loading && setMovementsList(movements);
-  }, [loading, movements]);
 
   const [movementsList, setMovementsList] = useState<PropertiesI[]>(movements);
   const filterPostive = () => {
@@ -50,42 +41,17 @@ export const HomeContainer = (): JSX.Element => {
     setIsFilter(false);
   };
   return (
-    <GenericContainer>
-      <Header name="Rubén Rodríguez" />
-      <PointsCard />
-      {loading && !success ? (
-        <ActivityIndicator size={'large'} />
-      ) : (
+    <SafeAreaView>
+      <GenericContainer>
+        <Header name="Rubén Rodríguez" />
+        <PointsCard />
         <MovementsComponentList itemList={movementsList!} />
-      )}
-
-      {isFilter ? (
-        <StyledButton onPress={withoutFilter}>
-          {i18n.t('everything')}
-        </StyledButton>
-      ) : (
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-evenly',
-            paddingHorizontal: 20,
-            width: '100%',
-            gap: 13,
-          }}>
-          <StyledButton
-            style={{ width: '50%' }}
-            sizeText="size12_800"
-            onPress={filterPostive}>
-            {i18n.t('win')}
-          </StyledButton>
-          <StyledButton
-            style={{ width: '50%' }}
-            sizeText="size12_800"
-            onPress={filterNegatives}>
-            {i18n.t('loss')}
-          </StyledButton>
-        </View>
-      )}
-    </GenericContainer>
+        <FilterButton
+          filterAll={withoutFilter}
+          filterNegative={filterNegatives}
+          filterPositive={filterPostive}
+          isFilter={isFilter} />
+      </GenericContainer>
+    </SafeAreaView>
   );
 };
